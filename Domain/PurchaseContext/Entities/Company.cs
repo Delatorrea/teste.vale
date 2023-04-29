8d6da5f4997255fe65e70450c81064ba72089e76
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-04-29 00:43:53 -0300

    🦄 refactor(Company):

--- Domain/PurchaseContext/Entities/Company.cs
+++ Domain/PurchaseContext/Entities/Company.cs
@@ -1,24 +1,13 @@
-using Domain.PurchaseContext.Entities.Contracts;
-using Domain.PurchaseContext.ValueObjects;
-using Flunt.Validations;
 using Shared.Entities;
+using Shared.ValueObjects;
 
 namespace Domain.PurchaseContext.Entities
 {
-    public class Company : Entity
+    public class Company : LegalEntity
     {
-        public TaxIdentifier TaxIdentifier { get; private set; }
-        public string TradeName { get; private set; } = string.Empty;
-        public ZipCode ZipCode { get; private set; }
 
-        public Company(TaxIdentifier taxIdentifier, string tradeName, ZipCode zipCode)
-        {
-            AddNotifications(new CreateCompanyContract(this));
-
-            this.TaxIdentifier = taxIdentifier;
-            this.TradeName = tradeName;
-            this.ZipCode = zipCode;
-        }
+        public Company(TaxIdentifier taxIdentifier, string tradeName, ZipCode zipCode) : base(taxIdentifier, tradeName, zipCode)
+        { }
 
     }
 }
