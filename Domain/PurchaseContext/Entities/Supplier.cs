b8c80bd12263746f728d819a137d73f203a0c0fb
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-04-29 00:44:13 -0300

    ✨ feat:
    Add Supplier Entity

---
+++ Domain/PurchaseContext/Entities/Supplier.cs
@@ -0,0 +1,14 @@
+using Shared.ValueObjects;
+
+namespace Domain.PurchaseContext.Entities
+{
+    public class Supplier : LegalEntity
+    {
+        public Supplier(TaxIdentifier taxIdentifier, string tradeName, Email email, ZipCode zipCode) : base(taxIdentifier, tradeName, zipCode)
+        {
+            this.Email = email;
+        }
+
+        public Email Email { get; private set; }
+    }
+}
