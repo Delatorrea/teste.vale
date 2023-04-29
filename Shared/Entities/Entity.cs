56b5bba2b35751213d0491aec19d9e499f569a3a
Emerson Delatorre <emerson.delatorre@avanade.com>
2023-04-28 23:43:38 -0300

    ✨ feat(Entity):
    Add Entity

---
+++ Shared/Entities/Entity.cs
@@ -0,0 +1,17 @@
+
+using Flunt.Notifications;
+
+namespace Shared.Entities
+{
+  public abstract class Entity : Notifiable<Notification>
+  {
+    public Entity()
+    {
+      this.Id = Guid.NewGuid();
+      this.CreationDate = DateTime.UtcNow;
+    }
+
+    public Guid Id { get; set; }
+    public DateTime CreationDate { get; set; }
+  }
+}
