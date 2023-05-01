de461c2ae71f3c76d3a76997c6d53b819c67df6d
Emerson Delatorre <delatorre@hotmail.com.br>
2023-05-01 10:12:33 -0300

    Add CompanyService

---
+++ Domain/PurchaseContext/Services/CompanyService.cs
@@ -0,0 +1,42 @@
+﻿
+using Domain.PurchaseContext.Entities;
+using Domain.PurchaseContext.Interfaces.Repositories;
+using Domain.PurchaseContext.Interfaces.Services;
+
+namespace Domain.PurchaseContext.Services
+{
+    public class CompanyService : ICompanyService
+    {
+        private readonly ICompaniesRepository _companiesRepository;
+
+        public CompanyService(ICompaniesRepository companiesRepository)
+        {
+            this._companiesRepository = companiesRepository;
+        }
+
+        public async Task Add(Company entity)
+        {
+            await this._companiesRepository.Add(entity);
+        }
+
+        public async Task Delete(Company entity)
+        {
+            await this._companiesRepository.Delete(entity);
+        }
+
+        public async Task<List<Company>> GetAll()
+        {
+            return await this._companiesRepository.GetAll();
+        }
+
+        public async Task<Company> GetById(int id)
+        {
+            return await this._companiesRepository.GetById(id);
+        }
+
+        public async Task Update(Company entity)
+        {
+            await this._companiesRepository.Update(entity);
+        }
+    }
+}
