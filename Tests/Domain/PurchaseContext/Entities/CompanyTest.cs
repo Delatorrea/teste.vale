86256b9dfa07510c6281dc998590856ddf575a9f
Emerson Delatorre <delatorre@hotmail.com.br>
2023-05-01 10:13:02 -0300

    Add CompanyTest

--- Tests/Domain/PurchaseContext/Entities/CompanyTest.cs
+++ Tests/Domain/PurchaseContext/Entities/CompanyTest.cs
@@ -7,13 +7,38 @@ namespace Tests.Domain.PurchaseContext.Entities
     [TestClass]
     public class CompanyTest
     {
-        [TestMethod]
-        public void Shold_return_valid_entity()
+        private readonly TaxIdentifier _cpfValid;
+        private readonly TaxIdentifier _cnpjValid;
+        private readonly TaxIdentifier _cnpjInvalid;
+        private readonly ZipCode _cepValid;
+
+        public CompanyTest()
         {
-            var cnpj = new TaxIdentifier("00.427.318/0001-53");
-            var cep = new ZipCode("44780-000");
-            var company = new Company(cnpj, "any_trade_name", cep);
+            this._cpfValid = new("05841241710");
+            this._cnpjValid = new("15105766000108");
+            this._cnpjInvalid = new("any_cnpj");
+            this._cepValid = new("44780-000");
+        }
+
+        [TestMethod]
+        public void Shold_return_true_when_valid_company_with_cnpj()
+        {
+            var company = new Company(this._cnpjValid, "any_trade_name", this._cepValid);
             Assert.IsTrue(company.IsValid);
+        }
+
+        [TestMethod]
+        public void Shold_return_false_when_invalid_company_with_cpf()
+        {
+            var company = new Company(this._cpfValid, "any_trade_name", this._cepValid);
+            Assert.IsFalse(company.IsValid);
+        }
+
+        [TestMethod]
+        public void Shold_return_false_when_invalid_company_with_cnpj()
+        {
+            var company = new Company(this._cnpjInvalid, "any_trade_name", this._cepValid);
+            Assert.IsFalse(company.IsValid);
         }
     }
 }
